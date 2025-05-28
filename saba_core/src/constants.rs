690a90e5d7fe2c08415ba5809b1b82c96b61dbe4
Teradad41 <sitiang120@gmail.com>
2025-05-29 00:52:48 +0900

    定数を追加

---
+++ saba_core/src/constants.rs
@@ -0,0 +1,18 @@
+// 色に関する定数
+pub static WHITE: u32 = 0xFFFFFFFF;
+pub static LIGHT_GRAY: u32 = 0xFFd3d3d3;
+pub static GRAY: u32 = 0xFF808080;
+pub static DARK_GRAY: u32 = 0xFF5A5A5A;
+pub static BLACK: u32 = 0xFF000000;
+
+pub static ADDRESS_BAR_HEIGHT: i64 = 20;
+
+pub static WINDOW_INIT_X_POS: i64 = 30;
+pub static WINDOW_INIT_Y_POS: i64 = 50;
+
+pub static WINDOW_WIDTH: i64 = 600;
+pub static WINDOW_HEIGHT: i64 = 400;
+pub static WINDOW_PADDING: i64 = 5;
+
+// noli ライブラリに定義されている定数
+pub static TOOLBAR_HEIGHT: i64 = 26;
